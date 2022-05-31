b9b9e50aaa00b1d09e2491aea50597371c85a12a
mriduldas07 <mriduldas0325@gmail.com>
2022-05-31 23:23:27 +0600

    services section

--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -3,6 +3,8 @@ import './Services.css';
 import HeartEmoji from "../../img/heartemoji.png";
 import Glasses from "../../img/glasses.png";
 import Humble from "../../img/humble.png";
+import Card from '../Card/Card';
+import resume from '../Services/resume.pdf'
 
 const Services = () => {
     return (
@@ -12,12 +14,36 @@ const Services = () => {
                 <span>My awesome</span>
                 <span>services</span>
                 <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor, voluptatum consectetur <br /> quia expedita quasi ad dicta fugit aliquam quae in ea deserunt consequuntur molestiae libero temporibus iusto nostrum officia quam!</span>
-                <button className="button s-button">Download CV</button>
+                <a href={resume} download className="button s-button">
+                Download CV
+                </a>   
                 <div className="blur s-blur1" style={{background: "#ABF1FF94"}}></div>
             </div>
             {/* right side */}
             <div className="cards">
-                im am right
+                {/* 1st card */}
+                <div style={{left:'14rem'}}>
+                    <Card
+                        emoji = {HeartEmoji}
+                        heading = {"Design"}
+                        detail = {"Figma, Sketch"}
+                    ></Card>
+                </div>
+                {/* 2nd card */}
+                <div style={{top: '12rem', left: '-4rem'}}>
+                    <Card
+                    emoji={Glasses}
+                    heading={'Developer'}
+                    detail={'Html, Css, Javascript,React'}
+                    ></Card>
+                </div>
+                <div style={{top: '19rem', left: '12rem'}}>
+                    <Card
+                    emoji={Humble}
+                    heading={'UI/UX'}
+                    detail={'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor, voluptatum consectetur'}
+                    ></Card>
+                </div>
             </div>
         </div>
     );
